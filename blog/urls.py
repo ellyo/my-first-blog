63473928eaa0f3306db572239064defa7273587f
Elly <elly.o@hotmail.co.uk>
2016-04-24 14:14:22 +0100

    Added mood pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^cocktailslist/$', views.post_list, name='post_list'),
+    url(r'^$', views.mood_list, name='homepage'),
+    url(r'^happy/$', views.happy_list, name='happy_list'),
+    url(r'^angry/$', views.angry_list, name='angry_list'),
+    url(r'^sad/$', views.sad_list, name='sad_list'),
+    url(r'^fun/$', views.fun_list, name='fun_list'),
+    url(r'^excited/$', views.excited_list, name='excited_list'),
+    url(r'^hungry/$', views.hungry_list, name='hungry_list'),
+    url(r'^lazy/$', views.lazy_list, name='lazy_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,36 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def mood_list(request):
+    moods = ['Happy', 'Angry', 'Sad']
+    return render(request, 'blog/mood_list.html', {'moods': moods})
+
+def happy_list(request):
+    posts = Post.objects.all().filter(mood__icontains='happy').order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def angry_list(request):
+    posts = Post.objects.all().filter(mood__icontains='angry').order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def sad_list(request):
+    posts = Post.objects.all().filter(mood__icontains='sad').order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def fun_list(request):
+    posts = Post.objects.all().filter(mood__icontains='fun').order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def excited_list(request):
+    posts = Post.objects.all().filter(mood__icontains='excited').order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def hungry_list(request):
+    posts = Post.objects.all().filter(mood__icontains='hungry').order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def lazy_list(request):
+    posts = Post.objects.all().filter(mood__icontains='lazy').order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
